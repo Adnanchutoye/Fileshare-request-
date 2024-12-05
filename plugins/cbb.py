ff2e57d86bfd800c12495406a3c183d7e89b3053
Adnanchutoye <mmjoker786@gmail.com>
2024-12-05 14:03:01 +0530

    Add files via upload

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,4 +1,4 @@
-#TG > @i_killed_my_clan
+#TELEGRAM > @ifeelscam
 
 from pyrogram import __version__
 from bot import Bot
@@ -10,13 +10,13 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>⌬ ᴏᴡɴᴇʀ : <a href=https://t.me/Madhav_S9>❰⏤͟͞ 𝗠.𝗥 𝗦𝗢𝗟𝗢-//-❱</a>\n⌬ ᴍʏ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/OngoingAnime_Htx'>ᴏɴɢᴏɪɴɢ ʜᴛx</a>\n⌬ ᴀɴɪᴍᴇ ɪɴᴅᴜsᴛʀʏ : <a href='https://t.me/anime_hindi_sub_industry'>ᴀɴɪᴍᴇ ɪɴᴅᴜsᴛʀʏ</a>\n⌬ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href='https://t.me/team_society_1'>ʙᴏᴛ sᴏᴄɪᴇᴛʏ</a>\n⌬ ʜᴛx ᴄʜᴀᴛ ᴢᴏɴᴇ : <a href='https://t.me/HTXANIMEGROUP'>ᴄʜᴀᴛ ᴢᴏɴᴇ</a>\n࿂ Dᴇᴠᴇʟᴏᴘᴇᴅ Bʏ : <a href='https://t.me/i_killed_my_clan'>❰⏤‌‌ 𝚯𝗕𝗜𝗧𝗢 -//-❱</a></b>",
+            text =f"<b>○ 𝐌ᴀɪɴ ᴄʜᴀɴɴᴇʟ : <a href='t.me/RyumaHindiSubAnime'>𝐑ʏᴜᴍᴀ 𝐀ɴɪᴍᴇ</a>\n○ 𝐎ɴɢᴏɪɴɢ ᴄʜᴀɴɴᴇʟ : <a href='https://t.me/RyumaOngoingAnime'>𝐎ɴɢᴏɪɴɢ 𝐑ʏᴜᴍᴀ</a>\n○ 𝐎ᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ : <a href='https://t.me/+YgEZxLLPXQo0YTg1'>𝐂ʟɪᴄᴋ ʜᴇʀᴇ</a>\n○ 𝐃ᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/Urr_Sanjiii'>𝐒ᴀɴJɪ 𝐒αᴍᴀ</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [ 
                   [
-                        InlineKeyboardButton("⛩️ Hᴏᴍᴇ", callback_data = "home"),
-                        InlineKeyboardButton("Cʟᴏsᴇ", callback_data = "close")
+                        InlineKeyboardButton("🔙 ʙᴀᴄᴋ ", callback_data = "home"),
+                        InlineKeyboardButton("🚫 ᴄʟᴏsᴇ ", callback_data = "close")
                     ]
                 ]
             )
@@ -29,14 +29,17 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             pass
     elif data == "home":
         await query.message.edit_text(
-            text=f"Hᴇʟʟᴏ!! {mention}⚡,\n\n<b>ɪ ᴀᴍ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ, ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.</a></b>",
+            text=f"Kᴏɴɴɪᴄʜɪᴡᴀ!! {mention}⚡,\n\n<b>ɪ ᴀᴍ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ, ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.\n\n𝐌ᴀɪɴ 𝐂ʜᴀɴɴᴇʟ : <a href='https://t.me/RyumaHindiSubAnime'>𝐑ʏᴜᴍᴀ 𝐀ɴɪᴍᴇ</a></b>",
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([
-                    [InlineKeyboardButton("• ғᴏʀ ᴍᴏʀᴇ •", url='https://t.me/anime_hindi_sub_industry')],
-                    [InlineKeyboardButton("• ᴀʙᴏᴜᴛ", callback_data='about'),
-                     InlineKeyboardButton("ʜᴇʟᴘ •", url='https://t.me/Anime_Support_Industry')],
-                    [InlineKeyboardButton("• ᴏᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ •", url='https://t.me/TEAM_HTX')],
-                ])
+            reply_markup = InlineKeyboardMarkup(
+                [
+                [
+                    InlineKeyboardButton("🤖 ᴀʙᴏᴜᴛ ᴍᴇ", callback_data = "about"),
+                    InlineKeyboardButton("🚫 ᴄʟᴏsᴇ ", callback_data = "close")
+                ]
+            ]
+            )
+        )
     elif data == "close":
         await query.message.delete()
         try:
@@ -50,9 +53,9 @@ async def cb_handler(client: Bot, query: CallbackQuery):
                 disable_web_page_preview=True,
                 reply_markup = InlineKeyboardMarkup(
                     [
-                        [ InlineKeyboardButton("Sᴜᴘᴘᴏʀᴛ",url = "https://t.me/HTXANIMEGROUP")],
-                        [ InlineKeyboardButton("⛩️ Hᴏᴍᴇ", callback_data = "home"),
-                         InlineKeyboardButton( "Cʟᴏsᴇ", callback_data = "close")]
+                        [ InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ",url = "https://t.me/Straw_Hat_Bots")],
+                        [ InlineKeyboardButton("🔙 ʙᴀᴄᴋ", callback_data = "home"),
+                         InlineKeyboardButton( "🚫 ᴄʟᴏsᴇ", callback_data = "close")]
                     ]
                 )
          )
